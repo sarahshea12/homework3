fe40767174a43946475432cb4e8a422572bbf5d2
Sarah Shealy <sshealy124@gmail.com>
2020-05-08 23:13:07 -0400

    made changes to loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-// Selections
+// User input
 var generateBtn = document.querySelector("#generate");
 var lowercase = confirm("Do you want your password to have lowercase letters?");
 var uppercase = confirm("Do you want your password to have uppercase letters?");
@@ -12,7 +12,40 @@ var lc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
 "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var sp = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-"]
+var selectCh = [0, 1, 2, 3]
 
+// Selections from arrays
+for (i=0; i < length; i++) {
+  // Selecting randomly which array to choose from
+  var selector3 = selectCh[Math.floor(Math.random() * 4)];
+  console.log(selector3);
+  // Select from lowercase array
+  var selector2 = Math.floor(Math.random() * 26);
+  if (selector3 === selectCh[1] && (lowercase)) {
+    var selectlc = lc[selector2];
+    console.log(selectlc);
+  }
+  // Select from uppercase array
+  var selector2 = Math.floor(Math.random() * 26);
+  if (selector3 === selectCh[2] && (uppercase)) {
+    var selectuc = uc[selector2];
+    console.log(selectuc);
+  }
+  // Select from number array
+  var selector1 = Math.floor(Math.random() * 10);
+  if (selector3 === selectCh[3] && (numeric)) {
+    var selectnum = num[selector1];
+    console.log(selectnum);
+  }
+  // Select from special character array
+  var selector1 = Math.floor(Math.random() * 10);
+  if (selector3 === selectCh[0] && (specialch)) {
+    var selectsp = sp[selector1];
+    console.log(selectsp);
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -26,10 +59,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//prompt for length of password; make it the i variable
-//do a loop so that it creats the number of characters the person wanted
-//make several arrays of the alphabet, numbers, etc
 // if <8 put in input, alert that there's not enough, same  with >128
 // if all are false, alert that they have not made a selection
 
 //make an alert of the result
+
+// at end, write "return.(name of function overall)"
+// blank array, insent choose letter, clear array
